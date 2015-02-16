776083bb4ff0945c374c3c200a121503ef0eba04
Makis Tracend <makis.tracend@gmail.com>
2015-02-16 06:06:05 -0800

    cookies option

--- config/options.js
+++ config/options.js
@@ -2,7 +2,8 @@
 var DEV = (process.env.NODE_ENV == "production") ? false : true;
 
 module.exports = {
-	body : true, //traditional body parser
+	body: true, //traditional body parser
+	cookies: true, //cookie parser
 	json: false, // supporting parsing of: application/json
 	files: false // parsing uploaded files
 }
